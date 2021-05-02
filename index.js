2be2d74df21950bdef1a05cfa6a38737bed3a453
Charis Kopsacheilis <charis.kopsacheilis@gmail.com>
2021-05-02 16:42:07 +0300

    Add puppeteer etc etc

--- index.js
+++ index.js
@@ -4,12 +4,14 @@
 
 const http = require('http');
 const url = require('url');
+const puppeteer = require('puppeteer');
+
 
 const PORT = 3001;
 
 
 // Create server
-http.createServer((req, res) => {
+http.createServer(async (req, res) => {
   const queryObj = url.parse(req.url, true).query;
 
   const pdfUrl = queryObj.pdfUrl;
@@ -21,9 +23,50 @@ http.createServer((req, res) => {
     return;
   }
 
-  res.write(`Hello, there`);
-  res.end();
+  try {
+    const pdf = await generatePDF(pdfUrl);
+
+    res.write(pdf);
+    res.end();
+  } catch (err) {
+    res.writeHead(500, 'Internal Server Error');
+    res.end(JSON.stringify({
+      message: err.message
+    }))
+  }
 })
 .listen(PORT, () => {
   console.log(`Server is listening on port: ${PORT}`);
-})
+});
+
+
+
+async function generatePDF(pdfUrl) {
+  
+  let browser;
+  const NODE_ENV = process.env.NODE_ENV;
+  if (!NODE_ENV || NODE_ENV === 'development') {
+    browser = await puppeteer.launch({
+      headless: true
+    })
+  } else {
+    browser = await puppeteer.launch({
+      headless: true,
+      executablePath: '/usr/bin/chromium-browser',
+      args: ['--no-sandbox']
+    })
+  }
+
+  const page = await browser.newPage();
+  await page.goto(pdfUrl, {
+    waitUntil: 'networkidle0'
+  });
+
+  const pdf = await page.pdf({
+    'format': 'a4'
+  })
+
+  browser.close();
+
+  return pdf;
+}
